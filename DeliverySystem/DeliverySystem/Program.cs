891f2a35d0ecf64a26bb402824c71f9f1e02ec80
atamancukdenis9 <atamancukdenis9@gmail.com>
2025-10-20 16:37:24 +0300

    add lab3

---
+++ DeliverySystem/DeliverySystem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DeliverySystem
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Vehicle scooter = new Scooter("Yamaha", 2020, 500, 20);
+            Vehicle car = new Car("Toyota", 2018, 20000, 4);
+            Vehicle van = new Van("Ford", 2019, 15000, 4, 1000);
+
+            Console.WriteLine(scooter.GetInfo());
+            Console.WriteLine($"Max Speed: {scooter.GetMaxSpeed()} km/h");
+            scooter.Move(20);
+            ((Scooter)scooter).Charge();
+
+            Console.WriteLine();
+            Console.WriteLine(car.GetInfo());
+            Console.WriteLine($"Max Speed: {car.GetMaxSpeed()} km/h");
+            car.Move(50);
+
+            Console.WriteLine();
+            Console.WriteLine(van.GetInfo());
+            Console.WriteLine($"Max Speed: {van.GetMaxSpeed()} km/h");
+            ((Van)van).LoadCargo(800);
+            Console.WriteLine(van.GetInfo());
+            ((Van)van).LoadCargo(300);
+            ((Van)van).UnloadCargo();
+        }
+    }
+}
---
+++ DeliverySystem/DeliverySystem/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DeliverySystem
+{
+    public class Vehicle
+    {
+        public string brand; 
+        public int year;
+        public double mileage;
+        public double maxSpeed;
+
+        public Vehicle(string brand, int year, double mileage, double maxSpeed)
+        {
+            this.brand = brand;
+            this.year = year;
+            this.mileage = mileage;
+            this.maxSpeed = maxSpeed;
+        }
+
+        public virtual string GetInfo()
+        {
+            // Тести, ймовірно, очікують саме цей формат
+            return $"{brand} ({year}), Mileage: {mileage} km";
+        }
+
+        public virtual double GetMaxSpeed()
+        {
+            return maxSpeed;
+        }
+
+        public virtual void Move(double distance)
+        {
+            mileage += distance;
+            Console.WriteLine($"{brand} drove {distance} km.");
+        }
+    }
+}
