66c55edf4e2ce7b18232e80c3d168831c76e1709
atamancukdenis9 <atamancukdenis9@gmail.com>
2025-11-03 17:30:08 +0200

    no message

---
+++ SmartHome/SmartHomeSystem/AirConditioner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeSystem
+{
+    public class AirConditioner:Device, IEnergyConsumer
+    {
+        private const int ACPowerConsumption = 2000; 
+        public string DeviceName => Name;
+        public int PowerConsumption => ACPowerConsumption;
+        public override void TurnOn()
+        {
+            IsOn = true;
+            Console.WriteLine($"{Name} почав охолодження.");
+        }
+        public override void TurnOff()
+        {
+            IsOn = false;
+            Console.WriteLine($"{Name} зупинено.");
+        }
+        public double GetEnergyUsage(int hours)
+        {
+            if(!IsOn)
+            {
+                return 0;
+            }
+            return (double)PowerConsumption * hours / 1000.0;
+        }
+    }
+}
---
+++ SmartHome/SmartHomeSystem/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartHomeSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var controller = new SmartHomeController();
+
+            var livingRoomLight = new Light { Name = "Лампа у вітальні" };
+            var bedroomAC = new AirConditioner { Name = "Кондиціонер у спальні" };
+            var kitchenCoffeeMachine = new CoffeeMachine { Name = "Кавомашина на кухні" };
+            var hallwaySensor = new MotionSensor { Name = "Датчик руху у коридорі" };
+
+            var allDevices = new List<Device> { livingRoomLight, bedroomAC, kitchenCoffeeMachine, hallwaySensor };
+
+            controller.AddDevice(livingRoomLight);
+            controller.AddDevice(bedroomAC);
+            controller.AddDevice(kitchenCoffeeMachine);
+            controller.AddDevice(hallwaySensor);
+
+            controller.AddEnergyDevice(livingRoomLight);
+            controller.AddEnergyDevice(bedroomAC);
+            controller.AddEnergyDevice(kitchenCoffeeMachine);
+
+            controller.TurnAllOn(); 
+
+            Console.WriteLine();
+
+            foreach (var device in allDevices)
+            {
+                device.PrintStatus(); 
+            }
+
+            Console.WriteLine();
+
+            controller.ShowEnergyReport(5);
+
+            controller.TurnAllOff(); 
+        }
+    }
+}
