33bf57cdd216d4b5049930570fd128877217e08c
atamancukdenis9 <atamancukdenis9@gmail.com>
2026-03-04 13:29:41 +0200

    no message

---
+++ prac2sem4(t1)/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public class TemperatureChangedEventArgs : EventArgs
+{
+    public float Temperature { get; }
+    public TemperatureChangedEventArgs(float temp) => Temperature = temp;
+}
+
+public class TemperatureSensor
+{
+    public event EventHandler<TemperatureChangedEventArgs> OnTemperatureChanged;
+
+    private float _currentTemperature;
+    public float CurrentTemperature
+    {
+        get => _currentTemperature;
+        set
+        {
+            _currentTemperature = value;
+            OnTemperatureChanged?.Invoke(this, new TemperatureChangedEventArgs(value));
+        }
+    }
+}
+
+public class Display
+{
+    public void OnUpdate(object sender, TemperatureChangedEventArgs e)
+    {
+        Console.WriteLine($"[Display] Temperature: {e.Temperature}°C");
+    }
+}
+
+public class AirConditioner
+{
+    public void OnUpdate(object sender, TemperatureChangedEventArgs e)
+    {
+        if (e.Temperature < 17)
+            Console.WriteLine("[AirConditioner] Heating ON");
+        else if (e.Temperature > 25)
+            Console.WriteLine("[AirConditioner] Cooling ON");
+        else
+            Console.WriteLine("[AirConditioner] OFF");
+    }
+}
+
+public class SecuritySystem
+{
+    public void OnUpdate(object sender, TemperatureChangedEventArgs e)
+    {
+        if (e.Temperature > 40)
+            Console.WriteLine("[SecuritySystem] OVERHEAT WARNING!");
+        else if (e.Temperature < 5)
+            Console.WriteLine("[SecuritySystem] FREEZE WARNING!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        TemperatureSensor sensor = new TemperatureSensor();
+        Display display = new Display();
+        AirConditioner ac = new AirConditioner();
+        SecuritySystem security = new SecuritySystem();
+
+        sensor.OnTemperatureChanged += display.OnUpdate;
+        sensor.OnTemperatureChanged += ac.OnUpdate;
+        sensor.OnTemperatureChanged += security.OnUpdate;
+
+        sensor.CurrentTemperature = 20;
+        sensor.CurrentTemperature = 10;
+        sensor.CurrentTemperature = 30;
+        sensor.CurrentTemperature = 45;
+        sensor.CurrentTemperature = 2;
+    }
+}
---
+++ prac2sem4(t2)/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+public class DamageEventArgs : EventArgs
+{
+    public int DamageTaken { get; }
+    public int CurrentHP { get; }
+    public DamageEventArgs(int damage, int hp)
+    {
+        DamageTaken = damage;
+        CurrentHP = hp;
+    }
+}
+
+public class Player
+{
+    public event EventHandler<DamageEventArgs> OnDamaged;
+    public int Health { get; private set; } = 100;
+
+    public void TakeDamage(int amount)
+    {
+        Health -= amount;
+        if (Health < 0) Health = 0;
+        OnDamaged?.Invoke(this, new DamageEventArgs(amount, Health));
+    }
+}
+
+public class UIHealthBar
+{
+    public void UpdateUI(object sender, DamageEventArgs e)
+    {
+        Console.WriteLine($"[UI] Health: {e.CurrentHP}%");
+    }
+}
+
+public class SoundSystem
+{
+    public void PlaySound(object sender, DamageEventArgs e)
+    {
+        Console.WriteLine("[Sound] Damage sound played");
+        if (e.CurrentHP <= 20 && e.CurrentHP > 0)
+            Console.WriteLine("[Sound] Critical health sound played");
+    }
+}
+
+public class AchievementSystem
+{
+    private bool _halfHealthReached = false;
+    private bool _firstDeathReached = false;
+
+    public void CheckAchievements(object sender, DamageEventArgs e)
+    {
+        if (e.CurrentHP <= 50 && !_halfHealthReached)
+        {
+            Console.WriteLine("[Achievement] Unlocked: Half Health");
+            _halfHealthReached = true;
+        }
+        if (e.CurrentHP <= 0 && !_firstDeathReached)
+        {
+            Console.WriteLine("[Achievement] Unlocked: First Death");
+            _firstDeathReached = true;
+        }
+    }
+}
+
+public class GameLogger
+{
+    public void LogData(object sender, DamageEventArgs e)
+    {
+        Console.WriteLine($"[Log] Damage: {e.DamageTaken}, HP: {e.CurrentHP}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Player player = new Player();
+        UIHealthBar ui = new UIHealthBar();
+        SoundSystem sound = new SoundSystem();
+        AchievementSystem achievements = new AchievementSystem();
+        GameLogger logger = new GameLogger();
+
+        player.OnDamaged += ui.UpdateUI;
+        player.OnDamaged += sound.PlaySound;
+        player.OnDamaged += achievements.CheckAchievements;
+        player.OnDamaged += logger.LogData;
+
+        player.TakeDamage(30);
+        player.TakeDamage(25);
+        player.TakeDamage(30);
+        player.TakeDamage(20);
+    }
+}
